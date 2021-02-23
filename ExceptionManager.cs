22fec6ea955e5e620decf119bc4891332bd7f39c
MarianaLoziak <loziak.marianna@gmail.com>
2021-02-23 22:51:05 +0200

    Add Lab1

---
+++ ExceptionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class ExceptionManager
+    {
+        public List<String> CriticalExceptions = new List<String>()
+        {
+            "System.NullReferenceException" ,
+            "System.ArgumentOutOfRangeException",
+            "System.IndexOutOfRangeException",
+            "System.OutOfMemoryException"
+        };
+
+
+        public bool IsExceptionCritical(Exception e)
+        {
+            String TypeOfException = e.GetType().ToString();
+            foreach (String item in CriticalExceptions)
+            {
+                if (TypeOfException == item)
+                    return true;
+            }
+            return false;
+
+        }
+
+        public void HandleExceptionMethod(Exception e, ref int CountCritical, ref int CountExceptional)
+        {
+            if (IsExceptionCritical(e))
+            {
+                CountCritical++;
+            }
+            else
+            {
+                CountExceptional++;
+            }
+        }
+
+    }
+}
---
+++ ExceptionManagerTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    [TestFixture]
+    class ExceptionManagerTests
+    {
+
+        [TestCaseSource(typeof(CasesClass), nameof(CasesClass.CriticalCases))]
+        public void IsExceptionCritical_CriticalException_ReturnsTrue(Exception e)
+        {
+            ExceptionManager exceptionManager = new ExceptionManager();//arrange
+
+            bool result = exceptionManager.IsExceptionCritical(e);//act
+
+            Assert.IsTrue(result);//assert
+        }
+
+        [TestCaseSource(typeof(CasesClass), nameof(CasesClass.NotCriticalCases))]
+        public void IsExceptionCritical_NotCriticalException_ReturnsFalse(Exception e)
+        {
+            ExceptionManager exceptionManager = new ExceptionManager();//arrange
+
+            bool result = exceptionManager.IsExceptionCritical(e);//act
+
+            Assert.IsFalse(result);//assert
+        }
+
+        [TestCaseSource(typeof(CasesClass), nameof(CasesClass.CriticalCases))]
+        public void HandleExceptionMethod_CriticalException_IncrementCriticalCounter(Exception e)
+        {
+            ExceptionManager exceptionManager = new ExceptionManager();
+            int count_exc = 0;
+            int count_critical = 0;
+
+            exceptionManager.HandleExceptionMethod(e, ref count_critical, ref count_exc);
+
+            Assert.AreEqual(count_critical, 1);
+            Assert.AreEqual(count_exc, 0);
+        }
+
+        [TestCaseSource(typeof(CasesClass), nameof(CasesClass.NotCriticalCases))]
+        public void HandleExceptionMethod_NotCriticalException_IncrementExceptionCounter(Exception e)
+        {
+            ExceptionManager exceptionManager = new ExceptionManager();
+            int count_exc = 0;
+            int count_critical = 0;
+
+            exceptionManager.HandleExceptionMethod(e, ref count_critical, ref count_exc);
+
+            Assert.AreEqual(count_critical, 0);
+            Assert.AreEqual(count_exc, 1);
+        }
+
+
+
+    }
+
+    public class CasesClass
+    {
+        public static object[] CriticalCases =
+        {
+            new ArgumentOutOfRangeException(),
+            new NullReferenceException(),
+            new IndexOutOfRangeException(),
+            new OutOfMemoryException()
+        };
+
+        public static object[] NotCriticalCases =
+        {
+            new ArgumentNullException(),
+            new DivideByZeroException(),
+            new SystemException(),
+            new FormatException(),
+            new KeyNotFoundException()
+
+
+        };
+    }
+
+}
